0efb5afa98a8930455eca93fff147df2e536c80c
Danylo <sukhiy37078@gmail.com>
2020-09-05 23:15:56 +0300

    Updates

---
+++ precache-manifest.d45501ecd890bdbff8985c4cd77745d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db23edcc8a86053ff4c0cc89aafa2b53",
+    "url": "/Danisimo2/react-hw-1/index.html"
+  },
+  {
+    "revision": "73e1b8d64d28bb7110ef",
+    "url": "/Danisimo2/react-hw-1/static/css/main.7b891105.chunk.css"
+  },
+  {
+    "revision": "14ab693299fa8b638e21",
+    "url": "/Danisimo2/react-hw-1/static/js/2.c8fae3a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Danisimo2/react-hw-1/static/js/2.c8fae3a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73e1b8d64d28bb7110ef",
+    "url": "/Danisimo2/react-hw-1/static/js/main.c0bdd07c.chunk.js"
+  },
+  {
+    "revision": "c2c7ead1d266db996596",
+    "url": "/Danisimo2/react-hw-1/static/js/runtime-main.97e54538.js"
+  }
+]);
